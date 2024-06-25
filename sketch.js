12fe1c474a7c8754575a3c51b3dc1469a7d6b459
SamuelKrakheckeDias <161588543+SamuelKrakheckeDias@users.noreply.github.com>
2024-06-25 08:10:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,7 @@
+var preimeiro_numero=prompt("digite primerio numero");
+var segundo_numero=prompt("digite segundo numero"):
+var terceiro_numero=prompt("digite terceiro numero")
+var quarto_numero=prompt("digite quarto numero")
+var quinto_numero=prompt("digite quinto numero")
+var resultado=number(primeiro_munero)+numer(segundo_mumero)-number(terceiro_numero)*number(quarto_numero)/number(quinto_numero)
+console.log("a soma e"+ resultado) 
